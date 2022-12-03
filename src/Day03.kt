7776c83fb0a540b852fe371043d213517582b4b7
Andreas Jonsson <andreas.jonsson@nav.no>
2022-12-03 07:37:20 +0100

    Solution day 3.

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,14 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun Char.getPriority(): Int =
+        if (this in 'a'..'z') {
+            this - 'a' + 1
+        } else {
+            this - 'A' + 27
+        }
+
+    fun part1(input: List<String>): Int =
+        input.sumOf { line ->
+            //Better way to split these?
+            val middleIndex = line.length / 2
+            val compartment1 = line.substring(0, middleIndex).map { it.getPriority() }
+            val compartment2 = line.substring(middleIndex).map { it.getPriority() }
+            compartment1.intersect(compartment2).first()
+        }
+
+    fun part2(input: List<String>): Int =
+        //How to intersect multiple sets in Kotlin?
+        input.chunked(3).sumOf { chunk ->
+            chunk.flatMap { it.toSet().map { char -> char.getPriority() } }
+                .groupBy { it }
+                .filter { it.value.size == 3 }
+                .keys.first()
+        }
 
     val testInput = readInputAsLines("Day03_test")
-    check(part1(testInput) == 2)
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInputAsLines("Day03")
     println(part1(input))
